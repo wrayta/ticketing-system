ba691ad2d410e4732d4e4d8cf1a3ec7c1f6b7807
wrayta <wrayta@hotmail.com>
2018-03-24 19:13:24 -0400

    Added docs directory containing info about db

--- ticketingsystem/settings.py
+++ ticketingsystem/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'ticketingsystem.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ticketing-system-db',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': '',
+        'PORT': '5432',
     }
 }
 
