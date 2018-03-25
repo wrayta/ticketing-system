e40256715a822ab76ee037ae139c4782af59bf8e
wrayta <wrayta@hotmail.com>
2018-03-25 13:45:12 -0400

    index 'hello, world' page

--- dashboard/models.py
+++ dashboard/models.py
@@ -2,12 +2,18 @@ from django.db import models
 from django.utils import timezone
 
 class System_User(models.Model):
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
+
     email = models.EmailField(max_length=200)
     password = models.CharField(max_length=200)
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
 
 class Ticket(models.Model):
+    def __str__(self):
+        return self.title
+
     title = models.CharField(max_length=200)
     description = models.CharField(max_length=2000)
     status = models.CharField(max_length=20, default='OPEN')
